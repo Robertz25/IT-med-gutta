80648429e82321a251e71dae6a9e17ff3ae81bc9
herms97 <35726959+herms97@users.noreply.github.com>
2018-03-08 13:15:02 +0100

    Update fileinfo.go

--- Oblig2/fileinfo.go
+++ Oblig2/fileinfo.go
@@ -3,14 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
-	"log"
 )
 
-
 func main() {
-	file, err := os.Open("file.go") // For read access.
+
+	file, err := os.Open("text.txt")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("couldn't open file: %s\n", err)
+		os.Exit(1)
 	}
-	fmt.Println("Information about file <" + file + ">:")
+
+	//Get stats
+	stats, err := file.Stat()
+	if err != nil {
+		fmt.Printf("couldn't get stats: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Size: %d bytes\n", stats.Size())
+	fmt.Printf("Is dir: %t\n", stats.IsDir())
+	fmt.Printf("Is regular: %t\n", stats.Mode().IsRegular())
+	fmt.Printf("Unix permissions: %s\n", stats.Mode().String())
+	fmt.Printf("Append only: %t\n", stats.Mode()&os.ModeAppend != 0)
+	fmt.Printf("Is device: %t\n", stats.Mode()&os.ModeDevice != 0)
+	fmt.Printf("Is symlink: %t\n", stats.Mode()&os.ModeSymlink != 0)
+
 }
