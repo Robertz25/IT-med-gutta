db7b6ca39d32794fa3c1af31e94bf367022a784c
herms97 <35726959+herms97@users.noreply.github.com>
2018-03-08 14:38:27 +0100

    Update fileinfo.go

--- Oblig2/fileinfo.go
+++ Oblig2/fileinfo.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 
-	file, err := os.Open("text.txt")
+	file, err := os.Open("fileinfo.go")
 	if err != nil {
 		fmt.Printf("couldn't open file: %s\n", err)
 		os.Exit(1)
 	}
+
+
+	datasize, err := ioutil.ReadFile("fileinfo.go")
+	if err != nil{
+		log.Panicf("failed at reading data from file: %s", err)
+	}
+	fmt.Printf("Size: %d bytes\n", len(datasize))
+	i := len(datasize)
+	f := float64(i)
+	fmt.Printf("Size: %f Kilobytes\n", f/1e3)
+	fmt.Printf("Size: %f Megabytes\n", f/1e6)
+	fmt.Printf("Size: %f Gigabytes\n", f/1e9)
+
 
 	//Get stats
 	stats, err := file.Stat()
@@ -20,7 +35,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Size: %d bytes\n", stats.Size())
 	fmt.Printf("Is dir: %t\n", stats.IsDir())
 	fmt.Printf("Is regular: %t\n", stats.Mode().IsRegular())
 	fmt.Printf("Unix permissions: %s\n", stats.Mode().String())
