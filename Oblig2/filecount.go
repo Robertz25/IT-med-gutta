9ab268999d2e5d07d1c5d63fd587fe4d38c0df6f
herms97 <35726959+herms97@users.noreply.github.com>
2018-03-09 13:16:57 +0100

    Update filecount.go

--- Oblig2/filecount.go
+++ Oblig2/filecount.go
@@ -1,21 +1,96 @@
 package main
+
 import (
-	"bufio"
+
 	"fmt"
+
+	"strings"
+
+	"io/ioutil"
+
+	"log"
+
+	"sort"
+
 	"os"
+	"bufio"
 )
 
 func main() {
+
+	fmt.Println("Information about <filecount.go>: \n")
+
+
 	file, _ := os.Open("text.txt")
 	fileScanner := bufio.NewScanner(file)
 	lineCount := 0
 	for fileScanner.Scan() {
 		lineCount++
 	}
-	fmt.Println("Number of lines in a file:", lineCount)
+	fmt.Println("Number of lines in a file: ", lineCount, "\n")
 
-	for  i := 1; i < 6; i++{
-			fmt.Println(i,".Rune:", i ," Counts:", lineCount)
+	fi, err := ioutil.ReadFile("text.txt")
+
+	Stringfile := string(fi)
+
+
+
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+
+	var characterCount = make(map[byte]int)
+
+	for i := 0; i < len(Stringfile); i++ {
+
+		if _, ok := characterCount[Stringfile[i]]; ok {
+
+
+
+		} else {
+
+			characterCount[Stringfile[i]] = strings.Count(Stringfile, string(Stringfile[i]))
+
 		}
 
 	}
+
+	type kv struct {
+
+		Key byte
+
+		Value int
+
+	}
+
+	var ss []kv
+
+	for k, v := range characterCount {
+
+		ss = append(ss, kv{k, v})
+
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+
+		return ss[i].Value > ss[j].Value
+
+	})
+
+	counter := 0
+
+	for _, kv := range ss {
+
+		if counter < 5 {
+
+			fmt.Printf("%d. Rune: %s, counts: %d\n", counter + 1, string(kv.Key), kv.Value)
+
+		}
+
+		counter++
+
+	}
+
+}
