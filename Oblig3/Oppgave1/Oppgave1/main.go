9ab4de22377ed30e7a0026d4b12ae33e442397e8
herms97 <35726959+herms97@users.noreply.github.com>
2018-04-23 17:12:03 +0200

    Update main.go

--- Oblig3/Oppgave1/Oppgave1/main.go
+++ Oblig3/Oppgave1/Oppgave1/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 	"net/http"
 	"log"
@@ -16,32 +15,12 @@ func CheckError(err error) {
 }
 
 
-    func HelloServer(w http.ResponseWriter, req *http.Request) {
-        fmt.Fprint(w, "Hello, client\n")
-  }
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello, client\n")
+}
 
 func main() {
-	ServerAddr,err := net.ResolveUDPAddr("udp",":8080")
-	CheckError(err)
 
-	ServerConn, err := net.ListenUDP("udp", ServerAddr)
-	CheckError(err)
-	defer ServerConn.Close()
-
-	buf := make([]byte, 1024)
-
-   http.HandleFunc("/", HelloServer)
-    log.Fatal(http.ListenAndServe(":8080", nil))
-
-
-
-	for {
-		n,addr,err := ServerConn.ReadFromUDP(buf)
-		fmt.Println("Received ",string(buf[0:n]), " from ",addr)
-
-		if err != nil {
-			fmt.Println("Error: ",err)
-		}
-		ServerConn.WriteToUDP([]byte("ack"), addr)
-	}
+	http.HandleFunc("/", HelloServer)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
