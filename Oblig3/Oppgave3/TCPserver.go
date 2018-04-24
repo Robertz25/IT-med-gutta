26491cfa413f4456a7108691141aff2057a331d9
Robertz25 <35694185+Robertz25@users.noreply.github.com>
2018-04-24 15:45:07 +0200

    Update TCPserver.go

--- Oblig3/Oppgave3/TCPserver.go
+++ Oblig3/Oppgave3/TCPserver.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-	"net"
-	"log"
-	"io"
 	"fmt"
+	"log"
+	"net"
+	"bufio"
 )
-
 func main() {
-li, err := net.Listen("tcp", ":8080")
-if err != nil {
-	log.Panic(err)
-}
-defer li.Close()
-
-for {
-	conn, err := li.Accept()
+	li, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Println(err)
+		log.Panic(err)
 	}
-	io.WriteString(conn, "\n Hello from TCP server \n")
-	fmt.Fprintln(conn, "How is your day")
-	fmt.Fprintf(conn, "%v", "Well I hope!")
+	defer li.Close()
 
-	conn.Close()
-}
+	for {
 
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
 }
+		func handle(conn net.Conn) {
+			scanner := bufio.NewScanner(conn)
+			for scanner.Scan() {
+				ln := scanner.Text()
+				fmt.Println(ln)
+				fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+		}
+	defer conn.Close()
+	fmt.Println("Code got here.")
+	}
+
+
